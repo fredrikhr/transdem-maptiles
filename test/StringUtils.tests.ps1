f7a3a7da34a5a7aa79e0388354f9600e5a8f304b
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-12-08 22:22:14 +0100

    Add Pester-tests for StringUtils and TrigonometryUtils

---
+++ test/StringUtils.tests.ps1
@@ -0,0 +1,35 @@
+Import-Module Pester
+Import-Module (Join-Path (Join-Path (Join-Path $PSScriptRoot "..") "src") "StringUtils.psm1")
+
+Describe "Get-Lines" {
+    It "Returns 1 non-empty string if given a single-line string" {
+        "This is a test" | Get-Lines | Should -Not -BeNullOrEmpty
+    }
+    It "Returns multiple strings if given a multi-line string using <SeparatorName> as line feed" -TestCases @(
+        @{ Separator = "`r"; SeparatorName = "CR" }
+        @{ Separator = "`n"; SeparatorName = "LF" }
+        @{ Separator = "`r`n"; SeparatorName = "CRLF" }
+    ) {
+        param ($Separator)
+        [string[]]$lines = (1..10)
+        $test = [string]::Join($Separator, $lines)
+        $test | Get-Lines | Should -Be $lines
+    }
+    It "Returns multiple trimmed strings if give a multi-line string and -Trim" {
+        [string[]]$lines = (1..10)
+        $writer = [System.Text.StringBuilder]::new()
+        foreach ($item in $lines) {
+            $writer.AppendLine(" `t$item `t")
+        }
+        $writer | Get-Lines -Trim | Should -Be $lines
+    }
+    It "Removes empty lines if given a multi-line string and -Options RemoveEmptyEntries" {
+        [string[]]$lines = (1..10)
+        $writer = [System.Text.StringBuilder]::new()
+        foreach ($item in $lines) {
+            $writer.AppendLine($item)
+            $writer.AppendLine([string]::Empty)
+        }
+        $writer | Get-Lines -Options RemoveEmptyEntries | Should -Be $lines
+    }
+}
---
+++ test/TrigonometryUtils.tests.ps1
@@ -0,0 +1,78 @@
+Import-Module Pester
+Import-Module (Join-Path (Join-Path (Join-Path $PSScriptRoot "..") "src") "TrigonometryUtils.psm1")
+
+Describe "Convert-DegreesToRadians" {
+    $examples = @(
+        @{ Value = 0; Expected = 0 }
+        @{ Value = 90; Expected = [Math]::PI * 0.5 }
+        @{ Value = 180; Expected = [Math]::PI }
+        @{ Value = 360; Expected = [Math]::PI * 2 }
+    )
+    It "Converts <Value> using the named parameter and returns <Expected>" -TestCases $examples {
+        param($Value, $Expected)
+
+        Convert-DegreesToRadians -Value $Value | Should -Be $Expected
+    }
+    It "Converts <Value> using the named parameter alias and returns <Expected>" -TestCases $examples {
+        param($Value, $Expected)
+
+        Convert-DegreesToRadians -Degrees $Value | Should -Be $Expected
+    }
+    It "Converts <Value> using the pipeline and returns <Expected>" -TestCases $examples {
+        param($Value, $Expected)
+
+        $Value | Convert-DegreesToRadians | Should -Be $Expected
+    }
+    It "Converts <Value> using the Value-property of an object in the pipeline and returns <Expected>" -TestCases $examples {
+        param($Value, $Expected)
+
+        [PSCustomObject]@{
+            Value = $Value
+        } | Convert-DegreesToRadians | Should -Be $Expected
+    }
+    It "Converts <Value> using the Degrees-property of an object in the pipeline and returns <Expected>" -TestCases $examples {
+        param($Value, $Expected)
+
+        [PSCustomObject]@{
+            Degrees = $Value
+        } | Convert-DegreesToRadians | Should -Be $Expected
+    }
+}
+
+Describe "Convert-RadiansToDegrees" {
+    $examples = @(
+        @{ Expected = 0; Value = 0 }
+        @{ Expected = 90; Value = [Math]::PI * 0.5 }
+        @{ Expected = 180; Value = [Math]::PI }
+        @{ Expected = 360; Value = [Math]::PI * 2 }
+    )
+    It "Converts <Value> using the named parameter and returns <Expected>" -TestCases $examples {
+        param($Value, $Expected)
+
+        Convert-RadiansToDegrees -Value $Value | Should -Be $Expected
+    }
+    It "Converts <Value> using the named parameter alias and returns <Expected>" -TestCases $examples {
+        param($Value, $Expected)
+
+        Convert-RadiansToDegrees -Radians $Value | Should -Be $Expected
+    }
+    It "Converts <Value> using the pipeline and returns <Expected>" -TestCases $examples {
+        param($Value, $Expected)
+
+        $Value | Convert-RadiansToDegrees | Should -Be $Expected
+    }
+    It "Converts <Value> using the Value-property of an object in the pipeline and returns <Expected>" -TestCases $examples {
+        param($Value, $Expected)
+
+        [PSCustomObject]@{
+            Value = $Value
+        } | Convert-RadiansToDegrees | Should -Be $Expected
+    }
+    It "Converts <Value> using the Radians-property of an object in the pipeline and returns <Expected>" -TestCases $examples {
+        param($Value, $Expected)
+
+        [PSCustomObject]@{
+            Radians = $Value
+        } | Convert-RadiansToDegrees | Should -Be $Expected
+    }
+}
