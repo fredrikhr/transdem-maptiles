ca763ce0d4e5b5ac4d903c81ed97247c8d70537c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-12-09 01:26:09 +0100

    Fix Errors in Test Result publishing

--- scripts/run-pester.ps1
+++ scripts/run-pester.ps1
@@ -1,35 +1,21 @@
-# Enable -Verbose option
 [CmdletBinding()]
+param ()
 
-$pesterTestPath = $null
-if (-not $Env:BUILD_SOURCESDIRECTORY) {
-    $pesterTestPath = Join-Path (Join-Path $PSScriptRoot "..") "test"
-} else {
-    $pesterTestPath = Join-Path $Env:BUILD_SOURCESDIRECTORY "test"
-}
+$pesterTestPath = Join-Path (Join-Path $PSScriptRoot "..") "test"
 $pesterTestPath = Join-Path $pesterTestPath "*"
 
-$outputFileName = $null
-if (-not $Env:BUILD_BUILDNUMBER) {
-    $dtNow = [datetime]::UtcNow
-    $dateString = $dtNow.ToString("yyyy-MM-dd")
-    $timeString = $dtNow.ToString("HH-mm-ss")
-    $outputFileName = "TEST-pester-build-unknown-date-$dateString-t-$timeString-Z.xml"
-} else {
-    $outputFileName = "TEST-pester-$Env:BUILD_BUILDNUMBER.xml"
-}
-$outputDirectory = $null
-if (-not $ENV:COMMON_TESTRESULTSDIRECTORY) {
-    $outputDirectory = Join-Path (Join-Path $PSScriptRoot "..") "TestResult"
-} else {
-    $outputDirectory = $ENV:COMMON_TESTRESULTSDIRECTORY
-}
+$outputFileName = "TEST-date-$dateString-t-$timeString-Z.xml"
+$outputDirectory = Join-Path (Join-Path $PSScriptRoot "..") "TestResult"
 if (-not (Test-Path $outputDirectory)) {
     New-Item -ItemType Directory $outputDirectory | Out-Null
 }
 $outputFilePath = Join-Path $outputDirectory $outputFileName
 
-Install-Module -Name Pester -Force -SkipPublisherCheck -Scope CurrentUser
-Import-Module Pester -Force
+Import-Module Pester
+$pesterModule = Get-Module Pester
+if ($pesterModule.Version -lt [version]::Parse("4.0")) {
+    Install-Module -Name Pester -Force -SkipPublisherCheck -Scope CurrentUser
+    Import-Module Pester -Force
+}
 Invoke-Pester -Script $pesterTestPath -EnableExit -OutputFormat NUnitXml `
     -OutputFile $outputFilePath
