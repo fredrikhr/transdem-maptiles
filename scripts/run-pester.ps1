1952bf062b83ec3fd409cd1447b3883f92220da4
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-02-28 13:18:43 +0100

    Add Install switch to Pester script

--- scripts/run-pester.ps1
+++ scripts/run-pester.ps1
@@ -1,16 +1,20 @@
 [CmdletBinding()]
-param ()
+param (
+    [switch]$Install = $false
+)
 
 if ([string]::IsNullOrWhiteSpace($Env:BUILD_SOURCESDIRECTORY)) {
     $pesterTestPath = Join-Path (Join-Path $PSScriptRoot "..") "test"
-} else {
+}
+else {
     $pesterTestPath = Join-Path $Env:BUILD_SOURCESDIRECTORY "test"
 }
 $pesterTestPath = Join-Path $pesterTestPath "*"
 
 if ([string]::IsNullOrWhiteSpace($Env:COMMON_TESTRESULTSDIRECTORY)) {
     $outputDirectory = Join-Path (Join-Path $PSScriptRoot "..") "TestResult"
-} else {
+}
+else {
     $outputDirectory = Join-Path $Env:COMMON_TESTRESULTSDIRECTORY "TestResult"
 }
 if ([string]::IsNullOrWhiteSpace($Env:BUILD_BUILDNUMBER)) {
@@ -27,11 +31,9 @@ if (-not (Test-Path $outputDirectory)) {
 }
 $outputFilePath = Join-Path $outputDirectory $outputFileName
 
-Import-Module Pester -ErrorAction Continue
-$pesterModule = Get-Module Pester
-if ($pesterModule.Version -lt [version]::Parse("4.0")) {
+if ($Install) {
     Install-Module -Name Pester -Force -SkipPublisherCheck -Scope CurrentUser
-    Import-Module Pester -Force
 }
+Import-Module Pester -Force
 Invoke-Pester -Script $pesterTestPath -EnableExit -OutputFormat NUnitXml `
     -OutputFile $outputFilePath
