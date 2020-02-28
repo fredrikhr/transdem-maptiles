a63f4982ac50c9d018c8e1e0afa6f34530dfd3ec
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-02-28 11:51:19 +0100

    Add UnitTest project

---
+++ test/Couven92.TransdemMaptiles.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Couven92.TransdemMaptiles.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
